278016e35f4ae60a3f6604426059e63a19654a13
ThomasVadis <o.thomasvadis@gmail.com>
2024-05-21 19:25:29 -0400

    made the models

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+  has_many :posts, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
